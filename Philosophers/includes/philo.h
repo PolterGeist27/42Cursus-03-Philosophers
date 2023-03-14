dd377b71421b81cbe757a10e2fca2b0bd9b8fd00
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-03-14 12:30:41 +0000

    first commit

---
+++ Philosophers/includes/philo.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diogmart <diogmart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 11:11:59 by diogmart          #+#    #+#             */
+/*   Updated: 2023/03/14 11:14:18 by diogmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include "libft.h"
+
+typedef struct s_data {
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_times_each_philosopher_must_eat;
+} t_data;
+
+#endif
