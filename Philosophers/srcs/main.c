6820468a4bffa251493f91fbe469adf667963e23
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-03-21 11:56:55 +0000

    added a free data function

--- Philosophers/srcs/main.c
+++ Philosophers/srcs/main.c
@@ -6,11 +6,18 @@
 /*   By: diogmart <diogmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 10:39:38 by diogmart          #+#    #+#             */
-/*   Updated: 2023/03/20 12:19:29 by diogmart         ###   ########.fr       */
+/*   Updated: 2023/03/21 11:54:03 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+void	free_data(t_data *data)
+{
+	free(data->forks);
+	free(data->philos);
+	free(data);
+}
 
 int	main(int argc, char **argv)
 {
@@ -20,7 +27,9 @@ int	main(int argc, char **argv)
 	if (!data || init(argc, argv, data))
 	{
 		printf("Error\n");
-		free(data);
+		free_data(data);
 		return (1);
 	}
+	init_simulation(data);
+	free_data(data);
 }
