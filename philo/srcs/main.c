b10fd552beef19bf24362f20df8a77ba12ee687d
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-03-28 14:53:50 +0100

    small fix

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: diogmart <diogmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 10:39:38 by diogmart          #+#    #+#             */
-/*   Updated: 2023/03/28 14:40:29 by diogmart         ###   ########.fr       */
+/*   Updated: 2023/03/28 14:51:26 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,8 @@
 
 void	free_data(t_data *data)
 {
-	if (data->forks != NULL)
 		free(data->forks);
-	if (data->philos != NULL)
 		free(data->philos);
-	if (data != NULL)
 		free(data);
 }
 
